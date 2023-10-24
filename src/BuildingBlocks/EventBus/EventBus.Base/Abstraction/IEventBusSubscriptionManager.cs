84785aa8147c7e8dc5b78757bd7f179379407aaa
Enes Doner <businessenesdoner@gmail.com>
2023-10-24 15:58:01 +0300

    EventBus.Base was written

--- src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBusSubscriptionManager.cs
@@ -1,34 +1,22 @@
 ﻿using EventBus.Base.Events;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EventBus.Base.Abstraction
 {
     public interface IEventBusSubscriptionManager
     {
-        bool IsEmpty { get; } //Subscriton Manager herhangi bir event'imizin olup olmadığına karar verecek.
+        bool IsEmpty { get; }
 
-        event EventHandler<string> OnEventRemoved; // Remove edildiğinde içerde bi event oluşturacağız.
-
-        void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>; //Subscription ekleme - Kısıtladık.
-
+        event EventHandler<string> OnEventRemoved;
+        void AddSubscription<T, TH>() where T : IntegrationEvent where TH : IIntegrationEventHandler<T>;
         void RemoveSubscription<T, TH>() where TH : IIntegrationEventHandler<T> where T : IntegrationEvent;
-
-        bool HasSubscriptionsForEvent<T>() where T : IntegrationEvent;
-
-        bool HasSubscriptionsForEvent(string eventName);
-
-        Type GetEventTypeByName(string eventName); //Event Name gönderildiğinde tipini geri göndereceğiz. İsminden Event'in kendisine ulaşmış oluyoruz.
-
+        bool HasSubscriptionForEvent<T>() where T : IntegrationEvent;
+        bool HasSubscriptionForEvent(string eventName);
+        Type GetEventTypeByName(string eventName);
         void Clear();
-
-        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent; //Dışarıdan gönderilen event'in bütün subscriptionlarını ve handlerlarını geri döneceğimiz metot için. 
-
+        IEnumerable<SubscriptionInfo> GetHandlersForEvent<T>() where T : IntegrationEvent;
         IEnumerable<SubscriptionInfo> GetHandlersForEvent(string eventName);
-
-        string GetEventKey<T>(); //Integration event için kullanılan Key- Routing Key gibi düşünebilirsin.
+        string GetEventKey<T>();
     }
 }
---
+++ src/BuildingBlocks/EventBus/EventBus.Base/EventBusConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base
+{
+    public class EventBusConfig
+    {
+        public int ConnectionRetryCount { get; set; } = 5; // bağlanırken 5 kere dene sonra hata fırlatacağız Re-Try mekanizması kurulacak bunun için. Network problemleri yaşandığı durumlarda can yakmaması için
+
+        public string DefaultTopicName { get; set; } = "SellinguddyEventBus"; //Default Topic name- dışarıdan verilmemesi durumda sistem hata almaması için.
+
+        public string EventBusConnectionString { get; set; } = String.Empty;
+        public string SubscriberClientAppName { get; set; } = String.Empty;
+        public string EventNamePrefix { get; set; } = String.Empty;
+
+        public string EventNameSuffix { get; set; } = "IntegrationEvent";
+
+        public EventBusType EventBusType { get; set; } = EventBusType.RabbitMQ;
+        public object Connection { get; set; }
+
+        public bool DeleteEventPrefix => !String.IsNullOrEmpty(EventNamePrefix);
+        public bool DeleteEventSuffix => !String.IsNullOrEmpty(EventNameSuffix);
+
+    }
+
+    public enum EventBusType
+    {
+        RabbitMQ = 0,
+        AzureServiceBus = 1
+
+    }
+}
