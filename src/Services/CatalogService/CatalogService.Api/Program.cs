5b7e04f3463e58c388a75f6c3eb93b568f28ec2b
Enes Doner <businessenesdoner@gmail.com>
2023-10-28 19:39:00 +0300

    Catalog Service changed

--- src/Services/CatalogService/CatalogService.Api/Program.cs
+++ src/Services/CatalogService/CatalogService.Api/Program.cs
@@ -1,15 +1,57 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 
-// Add services to the container.
+using CatalogService.Api.Infrastructure.Context;
+using Consul;
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ApplicationName = typeof(Program).Assembly.FullName,
+    ContentRootPath = Directory.GetCurrentDirectory(),
+    EnvironmentName = Environments.Staging,
+    WebRootPath = "pics"
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+builder.Services.AddDbContext<CatalogContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLConnection"));
+});
+builder.Services.AddTransient<CatalogContextSeed>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<CatalogContext>();
+    var env = services.GetRequiredService<IWebHostEnvironment>();
+    var logger = services.GetRequiredService<ILogger<CatalogContextSeed>>();
+
+    var contextSeed = new CatalogContextSeed();
+    contextSeed.SeedAsync(context, env, logger).Wait();
+ 
+    var db = scope.ServiceProvider.GetRequiredService<CatalogContext>();
+    db.Database.Migrate();
+   
+}
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +59,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
