22b7cf07afc09ebcfa3ddfecebae2fa7b0794e0c
Enes Doner <businessenesdoner@gmail.com>
2023-10-29 19:20:38 +0300

    Identity service was written

---
+++ src/Services/IdentityService/IdentityService.Api/Controllers/AuthContoller.cs
@@ -0,0 +1,28 @@
+﻿using IdentityService.Api.Application.Models;
+using IdentityService.Api.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IIdentityService IdentityService;
+
+        public AuthController(IIdentityService identityService)
+        {
+            IdentityService = identityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginRequestModel loginRequestModel)
+        {
+            var result = await IdentityService.LoginAsync(loginRequestModel);
+
+            return Ok(result);
+        }
+    }
+}
+
--- src/Services/IdentityService/IdentityService.Api/Program.cs
+++ src/Services/IdentityService/IdentityService.Api/Program.cs
@@ -1,3 +1,6 @@
+using IdentityService.Api.Application.Services;
+using System.Security.Principal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IIdentityService, IdentityService.Api.Application.Services.IdentityService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
